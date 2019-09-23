2c9aba56d16d6dff50aec768714ba8aa1dd36a67
joshua richards <jermain119@gmail.com>
2019-09-23 19:00:43 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,7 +1,7 @@
-# Your code here
+# takes in three parameters a,b,c
 def meal_choice(veg1, veg2, protein= "meat")
   puts "What a nutritious meal!"
   puts  "A plate of #{protein} with #{brocoli} and #{peas}." 
 end
-
+#returns values
 meal_choice()
